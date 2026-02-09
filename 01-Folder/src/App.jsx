52c8415d6d34f784f7b8e64947596eb7d680cfc5
vipin <vipin.shukla@guidesly.com>
2026-02-09 13:48:35 +0530

    Initial commit

---
+++ 01-Folder/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function App() {
+
+  return ( 
+  <> 
+    <div id='parent'>
+       <h1 id='child1'>Hello World</h1>
+       <h3 id='child2'>My Name is John Cena </h3>
+     </div>
+     <div>
+        <h2>I am wretler </h2>
+        <img src="https://reactjs.org/logo-180x180.png" alt="React Logo" />
+        <img src="" alt="" />
+     </div>
+  </>
+   )
+}
+
+
+
+
+export  default App 
